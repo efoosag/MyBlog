3a76fb15dfd380ba098aafedc002df43ad696201
efoosag <96612103+efoosag@users.noreply.github.com>
2022-09-25 14:07:24 +0100

    add views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
-  resources :posts, only: [:index, :show]
-  resources :users, only: [:index, :show]
-  root 'home#index'
-  
+  root 'users#index'  
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end  
 end
