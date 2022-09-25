537d66d3a1148c174ab71489131115ed72f01543
efoosag <96612103+efoosag@users.noreply.github.com>
2022-09-25 13:35:43 +0100

    implement routs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
-  resources :posts
-  resources :users
+  resources :posts, only: [:index, :show]
+  resources :users, only: [:index, :show]
   root 'home#index'
   
 end
