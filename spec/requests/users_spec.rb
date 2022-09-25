5c43030c47c6713e5ee20dc8a815cf52fe2126d8
efoosag <96612103+efoosag@users.noreply.github.com>
2022-09-25 20:14:49 +0100

    add users controller spec

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :request do
+  describe 'GET /index' do
+    before(:example) { get '/users'}
+
+    it 'Response Status To Be 200' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'Response Body To Include Correct placeholder' do
+      expect(response.body).to include('Index Page For Users')
+    end
+
+    it 'Response To include Correct Template' do
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET /show' do
+    before(:example) { get '/users/1'}
+
+    it 'Response Status To Be 200' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'Response Body To Include Correct placeholder' do
+      expect(response.body).to include('Display Page For User')
+    end
+
+    it 'Response To include Correct Template' do
+      expect(response).to render_template(:show)
+    end
+  end
+end
