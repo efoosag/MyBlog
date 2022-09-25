f4c02f4b863ede7fc70181aa8ec3d02933e48069
efoosag <96612103+efoosag@users.noreply.github.com>
2022-09-25 13:32:22 +0100

    delelte unnecessary file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  resources :likes
-  resources :comments
+  
   resources :posts
   resources :users
   root 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
 end
