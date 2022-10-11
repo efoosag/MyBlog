4ae12cfd89b7550dcab400849b305c9bcd6a895c
efoosag <96612103+efoosag@users.noreply.github.com>
2022-10-11 11:23:38 +0100

    complete project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,10 +8,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name password password_confirmation])
   end
-
-  rescue_from CanCan::AccessDenied do | exception |
-    redirect_to root_url, alert: exception.message
-  end
-
- 
 end
