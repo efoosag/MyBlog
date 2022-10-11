c13683c7398b1f7c15ee488a33aa0e09e7a2bc38
efoosag <96612103+efoosag@users.noreply.github.com>
2022-10-11 11:54:48 +0100

    resolve conflict errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -10,16 +9,3 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name password password_confirmation])
   end
 end
-=======
-class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  # protect_from_forgery unless: -> { request.format.json? }
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name password password_confirmation])
-  end
-end
->>>>>>> 6ead96391afbf69f1dafd7a617afbbd72d8f2725
