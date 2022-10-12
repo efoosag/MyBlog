21b7a9e3b0f08589fbe8ca4fbb1b96e2963c85b5
brainconnect93 <brainconnect0@gmail.com>
2022-10-12 08:31:40 +0100

    Make some change

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,18 +19,5 @@ class PostsController < ApplicationController
     params.require(:post).permit(:title, :text).merge(user: current_user, comments_counter: 0, likes_counter: 0)
   end
 
-  def create
-    respond_to do |format|
-      format.html do
-        @post = Post.new(post_params)
-        if @post.save
-          flash[:success] = 'Post Have Been Saved'
-          redirect_to user_post_path(current_user, @post)
-        else
-          flash.now[:error] = 'Error: Post could not be saved'
-          render :new, locals: { user: current_user, post: Post.new }
-        end
-      end
-    end
-  end
+ 
 end
